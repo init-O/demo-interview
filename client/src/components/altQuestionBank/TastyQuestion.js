0d257b76180183fb0e1d091d77201b9e5ad0719d
Saheb Kumar <sahebkumar026@gmail.com>
2022-01-12 19:02:03 +0530

    CSS updates for question bank

--- client/src/components/altQuestionBank/TastyQuestion.js
+++ client/src/components/altQuestionBank/TastyQuestion.js
@@ -3,11 +3,11 @@ import React from 'react'
 
 export default function altQuestion({question, level }) {
     return (
-        <div className="mt-5 pt-5 pl-2 mb-5 pb-3 rounded-full hover:bg-gray-200 flex justify-between cursor-pointer">
-           <div className="font-semibold">
+        <div className=" rounded-full bg-purple-100 m-1 hover:bg-gray-300 flex justify-between content-center items-center cursor-pointer">
+           <div className="pl-4 p-1 font-semibold content-center items-center">
             {question}
             </div> 
-            <button className={`bg-white hover:bg-gray-100  text-gray-800 font-semibold py-2 px-4 border ${level=='Junior'?'border-blue-400':level=='Middle'?'border-green-400':'border-red-400'} rounded-full shadow sm:hidden lg:inline`}>{level}</button>
+            <button className={` mr-4 bg-blue-100 hover:bg-gray-100  text-gray-800 font-semibold text-sm px-2  h-6 content-center  border ${level=='Junior'?'border-blue-400':level=='Middle'?'border-green-400':'border-red-400'} rounded-full shadow sm:hidden lg:inline`}>{level}</button>
         </div>
     )
 }
